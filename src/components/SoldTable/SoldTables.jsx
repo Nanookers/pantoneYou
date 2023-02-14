08eec565d0e3d66bc464641b361ffc8410d5f9da
Connor Clark <connordeclark@gmail.com>
2023-02-14 14:59:36 -0600

    Working on date dispatch for tables

--- src/components/SoldTable/SoldTables.jsx
+++ src/components/SoldTable/SoldTables.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// Set Table
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,12 +10,18 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { TableFooter } from '@mui/material';
 
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { Stack, TextField } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 function SoldTables() {
-
+        // soldTable Generator Begins //
         const dispatch = useDispatch(); 
         const soldTables = useSelector((store) => store.soldTables)
 
@@ -22,26 +29,56 @@ function SoldTables() {
 
         const sumOfSold = soldTables.reduce((accumulator, artPiece) => accumulator + Number(artPiece.price), 0);
         console.log(sumOfSold);
+        // soldTable Generator Ends //
 
-        // Handles populating the table with the relevent data.
-        function createData(name, calories, fat, carbs, protein) {
-          return { name, calories, fat, carbs, protein };
-        }
-        
-        // Handles the populating the content of the rows. 
-        const rows = [
-          createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-          createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-          createData('Eclair', 262, 16.0, 24, 6.0),
-          createData('Cupcake', 305, 3.7, 67, 4.3),
-          createData('Gingerbread', 356, 16.0, 49, 3.9),
-        ];
-      
+        const [value, setValue] = React.useState(dayjs());
+        const [endValue, setEndValue] = React.useState(dayjs());
+        console.log(value,endValue);
+
+        const handleChange = (newValue) => {
+          setValue(newValue);
+          const date = new Date(newValue)
+          const firstDay = date.getDate()
+          const firstMonth = date.getMonth()
+          const firstYear = date.getFullYear()
+          console.log(firstDay, firstMonth, firstYear);
+          dispatch({
+            type: "SAGA_FETCH_DATES"
+          })
+        };
+        const handleEndChange = (newValue) => {
+          setEndValue(newValue);
+          const date = new Date(newValue)
+          const firstDay = date.getDate()
+          const firstMonth = date.getMonth()
+          const firstYear = date.getFullYear()
+          console.log(firstDay, firstMonth, firstYear);
+        };
+
         useEffect(() => {
             dispatch({ type: 'SAGA_GET_ART_ACTIVE' });
           }, []);
       
         return (
+          <>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack spacing={3}>
+              <DesktopDatePicker
+                label="Start Day"
+                inputFormat="MM/DD/YYYY"
+                value={value}
+                onChange={handleChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <DesktopDatePicker
+                label="End Day"
+                inputFormat="MM/DD/YYYY"
+                value={endValue}
+                onChange={handleEndChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </Stack>
+        </LocalizationProvider>
           <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -71,11 +108,17 @@ function SoldTables() {
             </TableBody>
             <TableFooter>
               <TableRow>
+                <TableCell align="right"></TableCell>
                 <TableCell align="right">Total Sold:<span>{sumOfSold}</span></TableCell>
+                <TableCell align="right"></TableCell>
+                {/* Most Gallery Will Need to Be adjusted to show the most Earning Gallery */}
+                {/* <TableCell align="right">Most Gallery: <span>{soldTables[1].galleryName}</span></TableCell> */}
+                <TableCell align="right"></TableCell>
               </TableRow>
             </TableFooter>
           </Table>
         </TableContainer>
+        </>
         );
 }
 
